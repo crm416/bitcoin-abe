c645333072aa930f5703826857fd480c1f98ca18
Charles Marsh <charlie@khanacademy.org>
2014-12-03 15:35:59 -0500

    block_header_hash in PpcPosChain

--- Abe/Chain/PpcPosChain.py
+++ Abe/Chain/PpcPosChain.py
@@ -4,18 +4,19 @@
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public
 # License along with this program.  If not, see
 # <http://www.gnu.org/licenses/agpl.html>.
 
 from . import BaseChain
 from .. import deserialize
+from .. import util
 
 class PpcPosChain(BaseChain):
     """
@@ -28,3 +29,6 @@ class PpcPosChain(BaseChain):
         d = BaseChain.ds_parse_block(chain, ds)
         d['block_sig'] = ds.read_bytes(ds.read_compact_size())
         return d
+
+    def block_header_hash(chain, header):
+        return util.double_sha256(header)
